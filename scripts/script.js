2412893af2461adf293f1bd8987fcdb55ef78ec9
max-las <lasserre.maxime@yahoo.fr>
2020-10-22 10:45:15 +0200

    fix raccourci logo

--- scripts/script.js
+++ scripts/script.js
@@ -8,35 +8,59 @@ $( document ).ready(function() {
         $(e.target).attr("src", "images/slides/puce_pleine_blanche.png");
     });
 
+    shortcut_triggered = false;
+    $(".shortcut").click(function(e){
+        shortcut_triggered = true;
+    });
+
     event_decal_base = -1000;
-    $("#event_top").css("left", event_decal_base+"px");
-    $("#event_bottom").css("right", event_decal_base+"px");
     event_decal = event_decal_base;
+    setEventDecal(event_decal);
     reachedEvents = "no";
+    event_scroll_mark = 680;
     scrollCran = navigator.userAgent.includes("Firefox") ? 100 : 30;
     $(window).scroll(function(){
-        if(window.pageYOffset > 680 && reachedEvents=="no"){
-            reachedEvents = "yes";
-        }
-        if(window.pageYOffset < 680 && reachedEvents=="crossed"){
-            reachedEvents = "yes";
-        }
-        if(reachedEvents=="yes"){
-            if(window.pageYOffset > 680){
-                event_decal = event_decal + scrollCran;
+        if(!shortcut_triggered){
+            if(window.pageYOffset > event_scroll_mark && reachedEvents=="no"){
+                reachedEvents = "yes";
             }
-            if(window.pageYOffset < 680){
-                event_decal = event_decal - scrollCran;
+            if(window.pageYOffset < event_scroll_mark && reachedEvents=="crossed"){
+                reachedEvents = "yes";
             }
-            scrollTo(0,680);
-            $("#event_top").css("left", event_decal+"px");
-            $("#event_bottom").css("right", event_decal+"px");
-        }
-        if(event_decal < event_decal_base && reachedEvents=="yes"){
-            reachedEvents = "no";
-        }
-        if(event_decal > (event_decal_base * -1) && reachedEvents=="yes"){
-            reachedEvents = "crossed";
+            if(reachedEvents=="yes"){
+                if(window.pageYOffset > event_scroll_mark){
+                    event_decal = event_decal + scrollCran;
+                }
+                if(window.pageYOffset < event_scroll_mark){
+                    event_decal = event_decal - scrollCran;
+                }
+                scrollTo(0, event_scroll_mark);
+                setEventDecal(event_decal);
+            }
+            if(event_decal < event_decal_base && reachedEvents=="yes"){
+                reachedEvents = "no";
+            }
+            if(event_decal > (event_decal_base * -1) && reachedEvents=="yes"){
+                reachedEvents = "crossed";
+            }
+        }else{
+            shortcut_triggered = false;
+            event_decal = event_decal_base;
+            setEventDecal(event_decal);
+            if(window.pageYOffset > event_scroll_mark){
+                reachedEvents = "crossed";
+                event_decal = event_decal_base * -1;
+                setEventDecal(event_decal);
+            }else{
+                reachedEvents = "no";
+                event_decal = event_decal_base;
+                setEventDecal(event_decal);
+            }
         }
     });    
 });
+
+function setEventDecal(decal){
+    $("#event_top").css("left", decal+"px");
+    $("#event_bottom").css("right", decal+"px");
+}
